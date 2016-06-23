76fb120775ff7e9cf1bf88647ddefd37d29c7bf3
Guilherme Oliveira <guilherme.061@gmail.com>
2016-06-22 22:48:57 -0300

    trying to scrap some basic info on github

---
+++ Ruby/crawlers/github_scrapping.rb
@@ -0,0 +1,38 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'open_uri_redirections'
+
+# BASE LINK and github user
+GITHUB_BASE_LINK = "http://github.com"
+GITHUB_REPOSITORES = "?tab=repositories"
+GITHUB_ACTIVITY = "?tab=activity"
+GITHUB_USER = "guilhermepo2"
+
+# Where to save the github information from the user
+DATA_DIR = "github-scrapping/"
+Dir.mkdir(DATA_DIR) unless File.exists?(DATA_DIR)
+
+# Start Scrapping
+page_to_scrap = "#{GITHUB_BASE_LINK}/#{GITHUB_USER}"
+page = Nokogiri::HTML(open(page_to_scrap, :allow_redirections => :safe))
+
+# Getting info on github
+info = page.css("div.one-fourth")
+
+# Getting da names
+names = info.css("h1.vcard-names")
+puts "Name: " + names.css("div.vcard-fullname").text
+puts "username: " + names.css("div.vcard-username").text
+
+# Some infos about location, email, link and data
+infos = info.css("li.vcard-detail")
+infos.each do |i|
+  print i.attribute("aria-label").text + ": " + i.text + "\n"
+end
+
+more_info = info.css("a.vcard-stat")
+more_info.each do |t|
+  print t.css("strong.vcard-stat-count").text +
+        " " + t.css("span.text-muted").text + "\n"
+end
