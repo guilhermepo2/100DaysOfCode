7a2dd76408a161638d91d6271f47869b0a4b6f6f
Guilherme Oliveira <guilherme.061@gmail.com>
2016-06-28 14:29:24 -0300

    Simon Game

---
+++ Simon/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject green;
+	public GameObject red;
+	public GameObject blue;
+	public GameObject yellow;
+	List<int> simonSequence = new List<int>();
+
+	private void insertRandomIntoSequence()
+	{
+		simonSequence.Add (Random.Range(0,4));
+		Debug.Log ("Insertion!");
+	}
+
+	void FixedUpdate () 
+	{
+		if (Input.GetKeyDown (KeyCode.X))
+			insertRandomIntoSequence ();
+		if (Input.GetKeyDown (KeyCode.C))
+			StartCoroutine(SimonBoard.getInstance ().playSequence (simonSequence));
+	}
+}
